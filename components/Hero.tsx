bf0cb3aa53091c498cd88b33ccbd1c2138487607
Michael Di Giuseppe <mdigius@uwo.ca>
2023-11-23 12:18:55 -0500

    File structure created for next js app.

---
+++ components/Hero.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from 'react'
+import Image from 'next/image'
+import CustomButton from './CustomButton'
+const Hero = () => {
+    const handleScroll = () => {
+
+    }
+  return (
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+                Superhero Website!
+            </h1>
+            <p>
+                Sample subtitle
+            </p>
+            <CustomButton
+            title="Register"
+            containerStyles="bg-primary-blue
+            text-white rounded-full mt-10"
+            handleClick = {handleScroll}
+             />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
