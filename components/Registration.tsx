8c64409d8ddbb8392dd6ab0bacf0fbc96ace041b
Michael Di Giuseppe <mdigius@uwo.ca>
2023-11-23 13:59:56 -0500

    Imported flowbite library for user registration

--- components/Registration.tsx
+++ components/Registration.tsx
@@ -1,8 +1,10 @@
 "use client";
-
+import { Button, Checkbox, Label, Alert, TextInput } from "flowbite-react";
 import React from 'react'
 import Image from 'next/image'
 import CustomButton from './CustomButton'
+import Link from 'next/link';
+
 const Registration = () => {
     const handleScroll = () => {
 
@@ -16,12 +18,36 @@ const Registration = () => {
             <p>
                 Please enter an email and password below to register for our website.
             </p>
-            <CustomButton
-            title="Create Account"
-            containerStyles="bg-primary-blue
-            text-white rounded-full mt-10"
-            handleClick = {handleScroll}
-             />
+            <form className="flex max-w-md flex-col gap-4">
+                <div>
+                    <div className="mb-2 block">
+                    <Label htmlFor="email2" value="Email" />
+                    </div>
+                    <TextInput id="email2" type="email" placeholder="example@example.com" required shadow />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                    <Label htmlFor="password2" value="Password" />
+                    </div>
+                    <TextInput id="password2" type="password" required shadow />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                    <Label htmlFor="repeat-password" value="Confirm Password" />
+                    </div>
+                    <TextInput id="repeat-password" type="password" required shadow />
+                </div>
+                <div className="flex items-center gap-2">
+                    <Checkbox id="agree" />
+                    <Label htmlFor="agree" className="flex">
+                    I agree with the&nbsp;
+                    <Link href="#" className="text-cyan-600 hover:underline dark:text-cyan-500">
+                        terms and conditions
+                    </Link>
+                    </Label>
+                </div>
+                <Button type="submit">Register new account</Button>
+                </form>
         </div>
         <div className="hero__image-container">
                 <div className="hero__image">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -30,5 +31,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 };
